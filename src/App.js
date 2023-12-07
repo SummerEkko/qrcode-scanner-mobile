f3dcdd64fc83bc7a908411b2e85fb51adfa3a262
Kael <summr3ch0@gmail.com>
2023-12-06 18:55:12 -0600

    test1

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
-import React, { useState, useCallback } from 'react';
-import QRCodeGenerator from './pages/QRCodeGenerator';
-import QRCodeScanner from './pages/QRCodeScanner';
+import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import QRCodeScanner from './pages/QRCodeScanner';
 
 function App() {
-    const [scanning, setScanning] = useState(false);
+    const [isScanning, setIsScanning] = useState(false);
     const [scannedData, setScannedData] = useState('');
 
     const handleScan = (data) => {
         if (data) {
             setScannedData(data);
-            setScanning(false); // 停止扫描并关闭摄像头
+            setIsScanning(false); // 停止扫描
         }
     };
 
-    const toggleScan = useCallback(() => {
-        setScanning(!scanning);
-        if (scanning) {
-            setScannedData(''); // 清除上次扫描结果
-        }
-    }, [scanning]);
+    const toggleScanning = () => {
+        setIsScanning(!isScanning);
+    };
 
     return (
         <div className="App" style={{ padding: '20px' }}>
             <h1>QR Code Scanner</h1>
-            <Button variant="contained" onClick={toggleScan}>
-                {scanning ? 'Stop Scanning' : 'Start Scanning'}
+            <Button variant="contained" onClick={toggleScanning}>
+                {isScanning ? 'Stop Scanning' : 'Start Scanning'}
             </Button>
-            {scanning && <QRCodeScanner onScan={handleScan} />}
+            {isScanning && <QRCodeScanner onScan={handleScan} isScanning={isScanning} />}
             <TextField
                 fullWidth
                 label="Scanned Data"
--- src/pages/QRCodeScanner.js
+++ src/pages/QRCodeScanner.js
@@ -1,37 +1,71 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { BrowserMultiFormatReader } from '@zxing/library';
 
-function QRCodeScanner({ onScan }) {
+function QRCodeScanner({ onScan, isScanning }) {
     const videoRef = useRef(null);
+    const [hasPermission, setHasPermission] = useState(false);
     const codeReader = new BrowserMultiFormatReader();
 
     useEffect(() => {
-        const startScanner = async () => {
+        const requestCameraPermission = async () => {
             try {
-                const videoInputDevices = await BrowserMultiFormatReader.listVideoInputDevices();
-                if (videoInputDevices.length > 0) {
-                    const selectedDeviceId = videoInputDevices[0].deviceId;
-                    await codeReader.decodeFromVideoDevice(selectedDeviceId, videoRef.current, (result, err) => {
-                        if (result) {
-                            onScan(result.getText());
-                        } else if (err && !(err.name === 'NotFoundException')) {
-                            console.error(err);
-                        }
-                    });
+                // 设置约束使用后置摄像头
+                const constraints = {
+                    video: { facingMode: "environment" }
+                };
+
+                const stream = await navigator.mediaDevices.getUserMedia(constraints);
+                if (videoRef.current) {
+                    videoRef.current.srcObject = stream;
                 }
+                setHasPermission(true);
             } catch (error) {
-                console.error(error);
+                console.error('Camera access denied:', error);
+                setHasPermission(false);
             }
         };
 
-        startScanner();
+        const startScanner = async () => {
+            if (hasPermission && isScanning) {
+                try {
+                    const videoInputDevices = await BrowserMultiFormatReader.listVideoInputDevices();
+                    const selectedDeviceId = videoInputDevices.find(device => device.label.includes('back'))?.deviceId;
+                    
+                    if (selectedDeviceId) {
+                        await codeReader.decodeFromVideoDevice(selectedDeviceId, videoRef.current, (result, err) => {
+                            if (result) {
+                                onScan(result.getText());
+                                // 停止扫描和摄像头流
+                                videoRef.current.srcObject.getTracks().forEach(track => track.stop());
+                            } else if (err && !(err.name === 'NotFoundException')) {
+                                console.error(err);
+                            }
+                        });
+                    }
+                } catch (error) {
+                    console.error(error);
+                }
+            }
+        };
+
+        requestCameraPermission();
+        if (hasPermission && isScanning) {
+            startScanner();
+        }
 
         return () => {
             codeReader.reset();
+            if (videoRef.current && videoRef.current.srcObject) {
+                videoRef.current.srcObject.getTracks().forEach(track => track.stop());
+            }
         };
-    }, [onScan]);
+    }, [onScan, isScanning, hasPermission]);
 
-    return <video ref={videoRef} style={{ width: '100%' }} />;
+    return hasPermission ? (
+        <video ref={videoRef} style={{ width: '100%' }} autoPlay />
+    ) : (
+        <p>Camera access is required for scanning.</p>
+    );
 }
 
 export default QRCodeScanner;
